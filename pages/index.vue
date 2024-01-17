1bf791c2155d2d1bdad82b5810abe56402273ff2
tiago <tiago.muniz@scvsistemas.com.br>
2024-01-17 16:38:00 -0400

    feat: add component selects e arquivo constants

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        Index page!
-    </div>
+    <section class="flex items-center justify-between mb-10">
+        <h1 class="text-4xl font-extrabold">
+            Summary
+        </h1>
+        <div>
+            <USelectMenu :options="transactionViewOptions" v-model="viewSelected" />
+        </div>
+    </section>
 </template>
+
+<script setup>
+    import { transactionViewOptions } from '~/utils/constants'
+    const viewSelected = ref(transactionViewOptions[1])
+</script>
+
