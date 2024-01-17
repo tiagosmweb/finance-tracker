298bb293c9a2515d86f98caf4c037cffbd1c0948
tiago <tiago.muniz@scvsistemas.com.br>
2024-01-17 15:59:50 -0400

    feat: add estrutura inicial

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="flex justify-between items-center mt-10">
+        <nuxt-link to="/" class="text-xl font-bold">
+            Finance Tracker
+        </nuxt-link>
+        <div>
+             <UAvatar
+                size="sm"
+                src="https://avatars.githubusercontent.com/u/739984?v=4"
+                alt="Avatar"
+              />
+        </div>
+    </header>
+</template>
